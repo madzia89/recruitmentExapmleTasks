0d0ed04445fd40457650935f2819d4ce91411eed
madzia89 <wojcicka.ma@gmail.com>
2018-05-31 21:34:20 +0200

    5th task

--- script.js
+++ script.js
@@ -29,3 +29,43 @@ theFunction = (sth) => {
     }
 }
 
+//////////////////////////////////////////////////////////////////////
+// Zadanie 5
+// Napisz klasę javascriptową ​ Kot​ , która będzie w konstruktorze przyjmowała parametry
+// imie​ oraz ​
+// waga​ , oraz będzie posiadała metodę miaucz, która wypisze w konsoli:
+//     5Nazywam się <imię> i ważę <waga> kg
+// Dołącz kod JS do kodu z zadania 1, a w treści strony umieść przycisk ​ Dodaj kota​ , który
+// zapyta w wyskakującym oknie lub modalu (technika dowolna) o imię oraz o wagę i utworzy
+// nowego ​
+// kota z podanymi wartościami, a także utworzy przycisk ​ Miaucz <imie>​ , który
+// po kliknięciu uruchomi na danym ​ kocie​ metodę ​ miaucz​ .
+//
+Cat = (name, weight) => {
+    this.name = name
+    this.weight = weight
+    this.meow = console.log(`My name is ${name} and I weight ${weight}kg`)
+}
+
+const button = document.createElement('button')
+const firstDiv = document.createElement('div')
+document.body.appendChild(firstDiv)
+firstDiv.appendChild(button)
+button.innerText = 'clickMe'
+
+
+nameFunction = () => {
+    return prompt('type the name')
+}
+weightFunction = () => {
+    return prompt('type the weight')
+}
+button.addEventListener('click', () => {
+    nameFunction()
+    weightFunction()
+})
+Cat(nameFunction(), weightFunction())
+
+
+
+
